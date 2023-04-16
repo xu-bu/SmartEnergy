14e5eb35395c16c5b134bb3d7d9b42d40040ddc0
xu-bu <36945794+xu-bu@users.noreply.github.com>
2023-04-16 23:13:25 +0100

    change request queue

--- MainContract.sol
+++ MainContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 import "./P2P.sol"; 
 
@@ -12,7 +12,8 @@ interface IP2P {
     function buy(int energy,address userAddress) external ; 
     function checkIfRegistered(address requestAddress) view external  returns (bool);
     function topUp(address userAddress,uint256 amount) external  payable;
-     function getRate() external  view returns (int);
+    function getRate() external  view returns (int);
+    function withDraw(address payable  userAddress) external  payable;
 }
 
 
@@ -20,54 +21,59 @@ contract MainContract{
     address public owner;
     address P2PcontractAddress;
 
-    // the first person to deploy the contract is
-    // the owner
-    constructor() public {
-        owner = msg.sender;
-    }
+    // // the first person to deploy the contract is
+    // // the owner
+    // constructor() public {
+    //     owner = msg.sender;
+    // }
 
     function getP2P(address P2Paddress) public {
         P2PcontractAddress=P2Paddress;
     }
 
-    function register(string memory _name) public {
+    function register() public {
         IP2P(P2PcontractAddress).addProsumer(msg.sender);
     }
 
-    function checkEnergyStatus() public onlyRegitered view returns (int) {
+    function checkEnergyStatus() public onlyRegistered view returns (int) {
         return IP2P(P2PcontractAddress).checkEnergyStatus(msg.sender);
     }
 
-    function checkBalance() public onlyRegitered view returns (uint256) {
+    function checkBalance() public onlyRegistered view returns (uint256) {
         return IP2P(P2PcontractAddress).checkBalance(msg.sender);
     }
 
-    function energyRequestHandler(int energy) public onlyRegitered enoughEnergy(energy) {
+    function energyRequestHandler(int energy) public onlyRegistered enoughEnergy(energy) {
         if(energy>0){
             IP2P(P2PcontractAddress).sell(energy,msg.sender);
         }else if (energy<0){
-            IP2P(P2PcontractAddress).buy(energy,msg.sender);
+            IP2P(P2PcontractAddress).buy(-energy,msg.sender);
         }
     }
 
-    function topUp() public onlyRegitered payable{
+    function topUp() public onlyRegistered payable{
         IP2P(P2PcontractAddress).topUp(msg.sender,msg.value);
     }
 
+    function withdraw() public payable onlyRegistered {
+        IP2P(P2PcontractAddress).withDraw(payable (msg.sender));
+    }
+
     function testRegister() public {
-        register("a");
-        register("b");
-        register("c");
+        register();
+        register();
+        register();
     }
 
-    //modifier: https://medium.com/coinmonks/solidity-tutorial-all-about-modifiers-a86cf81c14cb
-    modifier onlyOwner() {
-        //is the message sender owner of the contract?
-        require(msg.sender == owner);
-        _;
-    }
 
-    modifier onlyRegitered(){
+    // //modifier: https://medium.com/coinmonks/solidity-tutorial-all-about-modifiers-a86cf81c14cb
+    // modifier onlyOwner() {
+    //     //is the message sender owner of the contract?
+    //     require(msg.sender == owner);
+    //     _;
+    // }
+
+    modifier onlyRegistered(){
         require(IP2P(P2PcontractAddress).checkIfRegistered(msg.sender),"You are not registered yet!");
         _;
     }
--- P2P.sol
+++ P2P.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 contract P2P{
 
     struct Prosumer{
         address id;
         uint256 balance;
+        // +: want to sell
+        // -: want to buy
         int energyStatus;
     }
 
     Prosumer[] public Prosumers;
+    address[] public sellers;
+    address[] public buyers;
     mapping(address => uint256) public address2Index;
     // rate between energy and token
-    int  public rate=1;
+    int  public rate=1000;
+    int cashbackNum=100;
+    
 
     function getRate() public view returns (int){
         return rate;
@@ -40,67 +46,104 @@ contract P2P{
         Prosumers[index].balance+=amount;
     }
 
-    function withDraw(address userAddress) public payable {
+    function withDraw(address payable userAddress) public payable {
         uint256 index=address2Index[userAddress];
         require(Prosumers[index].energyStatus>=0,"You are currently requuesting for buying energy, cannot withdraw!");
+        userAddress.transfer(Prosumers[index].balance);
         Prosumers[index].balance=0;
     }
 
-    function sell(int energy,address userAddress) public {
-        uint256 index=address2Index[userAddress];
-        require(Prosumers[index].energyStatus>0 && Prosumers[index].energyStatus>energy,"You don't have enough energy on your account, cannot sell!");
-        for (uint256 i=0; i<Prosumers.length; ++i) 
-        {
-            if (i==index || Prosumers[i].energyStatus>0){
-                continue ;
-            }
-            int tmp=energy+Prosumers[i].energyStatus;
-            if(tmp>0){
-                uint256 amount=uint256((-Prosumers[i].energyStatus)*rate);
-                Prosumers[index].balance+=amount;
-                Prosumers[i].balance-=amount;
-                Prosumers[index].energyStatus=tmp;
-                Prosumers[i].energyStatus=0;
-                energy=tmp;
-            }else{
-                Prosumers[index].balance+=uint256(energy*rate);
-                Prosumers[i].balance-=uint256(energy*rate);
-                Prosumers[i].energyStatus=tmp;
-                Prosumers[index].energyStatus=0;
-                energy=0;
-            }
-        }
-        // after iterating every prosumer, still remains some energy
-        Prosumers[index].energyStatus+=energy;
+    function addBuyer(address buyer) public {
+        buyers.push(buyer);
     }
 
-    function buy(int energy,address userAddress) public {
-        uint256 index=address2Index[userAddress];
-        require(Prosumers[index].balance>uint256(rate*(-energy)),"You don't have enough money on your account, cannot sell!");
-        for (uint256 i=0; i<Prosumers.length; ++i)
-        {
-            if (i==index || Prosumers[i].energyStatus<0){
-                continue ;
-            }
-            int tmp=energy+Prosumers[i].energyStatus;
-            if(tmp<0){
-                uint256 amount=uint256(Prosumers[i].energyStatus*rate);
+    function addSeller(address seller) public {
+        sellers.push(seller);
+    }
+
+    // The trader can get cashback based on the order trading volume
+    function incentive(address user, uint256 money)public {
+        uint256 index=address2Index[user];
+        Prosumers[index].balance+=money;
+    }
+
+    function sell(int energy,address userAddress) public {
+        uint256 seller=address2Index[userAddress];
+        require(Prosumers[seller].energyStatus>0 && Prosumers[seller].energyStatus>energy,"You don't have enough energy on your account, cannot sell!");
+        incentive(userAddress, uint256(energy*rate/cashbackNum));
+        for (uint256 i=0; i<buyers.length; ++i)
+        {   
+            uint256 index=address2Index[buyers[i]];
+            // if this buyer cannot fulfill our request
+            if(-Prosumers[index].energyStatus<energy){
+                energy+=Prosumers[index].energyStatus;
+                uint256 amount=uint256((-Prosumers[index].energyStatus)*rate);
+                Prosumers[seller].balance+=amount;
                 Prosumers[index].balance-=amount;
-                Prosumers[i].balance+=amount;
-                Prosumers[index].energyStatus=tmp;
-                Prosumers[i].energyStatus=0;
-                energy=tmp;
-            }else{
-                // I need 3 (energy=-3), Prosumers[i] is 6
-                Prosumers[index].balance+=uint256(energy*rate);
-                Prosumers[i].balance-=uint256(energy*rate);
-                Prosumers[i].energyStatus=tmp;
+                // Prosumers[seller].energyStatus=tmp;
                 Prosumers[index].energyStatus=0;
+            }else{
+            // if this buyer can fulfill our request
+                Prosumers[seller].balance+=uint256(energy*rate);
+                Prosumers[index].balance-=uint256(energy*rate);
+                Prosumers[index].energyStatus-=energy;
+                Prosumers[seller].energyStatus=0;
                 energy=0;
+                break;
             }
         }
-        // after iterating every prosumer, still need some energy
-        Prosumers[index].energyStatus+=energy;
+        Prosumers[seller].energyStatus+=energy;
+        sellers=new address[](0);
+        buyers=new address[](0);
+        for (uint256 i=0; i<Prosumers.length;++i) 
+        {
+            if (Prosumers[i].energyStatus>0){
+                sellers.push(Prosumers[i].id);
+            }else if(Prosumers[i].energyStatus<0){
+                buyers.push(Prosumers[i].id);
+            }
+        }
+    }
+
+    // the parameter is the amount of energy
+    function buy(int energy,address userAddress) public {
+        uint256 buyer=address2Index[userAddress];
+        require(Prosumers[buyer].balance>=uint256(rate*energy),"You don't have enough money on your account, cannot sell!");
+        incentive(userAddress, uint256(energy*rate/cashbackNum));
+        Prosumers[buyer].energyStatus-=energy;
+        for (uint256 i=0; i<sellers.length; ++i)
+        {   
+            uint256 index=address2Index[buyers[i]];
+            // if this seller cannot fulfill our request
+            if(Prosumers[index].energyStatus<energy){
+                energy-=Prosumers[index].energyStatus;
+                uint256 amount=uint256(Prosumers[index].energyStatus*rate);
+                Prosumers[buyer].balance-=amount;
+                Prosumers[index].balance+=amount;
+                // Prosumers[seller].energyStatus=tmp;
+                Prosumers[index].energyStatus=0;
+            }else{
+            // if this buyer can fulfill our request
+                Prosumers[buyer].balance-=uint256(energy*rate);
+                Prosumers[index].balance+=uint256(energy*rate);
+                Prosumers[index].energyStatus+=energy;
+                Prosumers[buyer].energyStatus=0;
+                energy=0;
+                break;
+            }
+        }
+        // cannot be fulfilles, still need to buy more
+        Prosumers[buyer].energyStatus-=energy;
+        sellers=new address[](0);
+        buyers=new address[](0);
+        for (uint256 i=0; i<Prosumers.length;++i) 
+        {
+            if (Prosumers[i].energyStatus>0){
+                sellers.push(Prosumers[i].id);
+            }else if(Prosumers[i].energyStatus<0){
+                buyers.push(Prosumers[i].id);
+            }
+        }
     }
 
     function checkEnergyStatus(address userAddress) public view returns(int){
